8181092616b21c8cbd5338f9a156db2e6bd2febe
Michael Moravec <mail@majkl578.cz>
2013-12-14 17:37:08 +0100

    PhpGenerator\ClassType: Fix getting class methods

--- Nette/PhpGenerator/ClassType.php
+++ Nette/PhpGenerator/ClassType.php
@@ -106,7 +106,7 @@ class ClassType extends Nette\Object
 			$class->properties[$prop->getName()] = Property::from($prop);
 		}
 		foreach ($from->getMethods() as $method) {
-			if ($method->getDeclaringClass() == $from) { // intentionally ==
+			if ($method->getDeclaringClass()->getName() === $from->getName()) {
 				$class->methods[$method->getName()] = Method::from($method);
 			}
 		}
